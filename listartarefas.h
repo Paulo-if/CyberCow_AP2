0ce66c97e5abb13cea90a1551f7b09d3ce153943
paulocosta <167147870+Paulo-if@users.noreply.github.com>
2024-12-07 09:15:04 -0300

    Add files via upload

---
+++ listartarefas.h
@@ -0,0 +1,28 @@
+#ifndef LISTARTAREFAS_H_INCLUDED
+#define LISTARTAREFAS_H_INCLUDED
+#define MAX_TAREFA 256
+
+void listarTarefas()
+{
+    FILE *arquivo = fopen("tarefas.txt", "r");
+    if (arquivo == NULL)
+    {
+        printf("Nenhuma tarefa encontrada!\n");
+    }
+
+    char tarefa[MAX_TAREFA];
+
+    printf("Tarefas:\n");
+    printf("-----------------------------\n\n");
+
+    while (fgets(tarefa, MAX_TAREFA, arquivo))
+    {
+        printf("- %s", tarefa);
+    }
+    printf("\n-----------------------------\n");
+
+    fclose(arquivo);
+}
+
+
+#endif // LISTARTAREFAS_H_INCLUDED
